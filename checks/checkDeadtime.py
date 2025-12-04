0f04ce7137a7c59b4784241dc1c3bc64e5ec5810
sthancock <steven.hancock@ed.ac.uk>
2025-12-04 11:12:37 +0000

    Dead-time script completed.

--- checks/checkDeadtime.py
+++ checks/checkDeadtime.py
@@ -5,7 +5,7 @@ to give a short effective-dead time
 '''
 
 from scipy.constants import c
-from scipy.stats import norm
+from scipy.stats import norm,poisson
 import argparse
 
 
@@ -15,7 +15,7 @@ def readCommands():
   '''
   Read commandline arguments
   '''
-  p = argparse.ArgumentParser(description=("Gives probability of detecting all photons for different pixel numbers for a given detector dead-time and signal rate"))
+  p = argparse.ArgumentParser(description=("Gives probability of not detecting all photons for a given detector dead-time and signal rate"))
   p.add_argument("--nPhotons",dest="nPhotons",type=int,default=205,help=("Number of expected signal photons\nDefault 205"))
   p.add_argument("--window",dest="window",type=float,default=20,help=("Measurement window in metres, eg. tree height\nDefault 20 m"))
   p.add_argument("--deadtime",dest="deadtime",type=float,default=2,help=("Pixel deadtime in ns\nDefault 2 ns"))
@@ -32,16 +32,13 @@ if __name__ == '__main__':
   cmd=readCommands()
   nPhotons=cmd.nPhotons
   window=cmd.window*2/c         # convert to two way time
-  mu=window/nPhotons            # distribtion mean. It's a Poisson so stdev=mu
   deadtime=cmd.deadtime*10**-9  # convert to seconds
 
+  # intermediate parameters
+  mu=window/nPhotons            # distribtion mean. It's a Poisson so stdev=mu
+  expected=deadtime/mu   # the expected number of photons within the deadtime
+  probProblem=poisson.cdf(1,expected)  # probability of more than 1 photon arriving within deadtime
 
-
-  meanSep=window/nPhotons
-  probDeadPer=norm.cdf((deadtime-mu)/mu)
-
-  # loop over number of pixels
-  for nPix in range(1,100):
-    probDeadArr=1-nPhotons*probDeadPer/nPix**2   # probability that no two photons are within the dead time of each other on the same pixel
-    print(nPix,'ProbArrive',round(probDeadPer,4),'probAll',round(probDeadArr,2))
+  print('We expect',round(expected,1),'photons to arrive within the deadtime')
+  print('The probability of not being affected by deadtime is',round(probProblem*100,2),'%')
 
